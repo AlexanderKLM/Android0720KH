eb50e2ae8d760c85abf21018c232f4be8795e15e
AlexanderKLM <alex57net@mail.ru>
2022-05-15 21:09:15 +0300

    M7

---
+++ app/src/main/java/ru/mirea/khelimskiy/mireaproject/HttpUrl.java
@@ -0,0 +1,177 @@
+package ru.mirea.khelimskiy.mireaproject;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HttpUrl#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HttpUrl extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private TextView resultTV;
+    private Button b2;
+    private String url = "https://www.google.ru/";
+
+    public HttpUrl() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HttpUrl.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HttpUrl newInstance(String param1, String param2) {
+        HttpUrl fragment = new HttpUrl();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View view1 = inflater.inflate(R.layout.fragment_http_url, container, false);
+b2 = view1.findViewById(R.id.buttonN);
+        resultTV = view1.findViewById(R.id.textView1);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectivityManager connectivityManager =
+                        (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkinfo = null;
+                if (connectivityManager != null) {
+                    networkinfo = connectivityManager.getActiveNetworkInfo();
+                }
+                if (networkinfo != null && networkinfo.isConnected()) {
+                    new DownloadPageTask().execute(url); // запускаем в новом потоке
+                } else {
+                    Toast.makeText(getContext(), "Нет интернета", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return view1;
+    }
+
+private class DownloadPageTask extends AsyncTask<String, Void, String> {
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        resultTV.setText("Загружаем...");
+    }
+    @Override
+    protected String doInBackground(String... urls) {
+        try {
+            return downloadIpInfo(urls[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+    @Override
+    protected void onPostExecute(String result) {
+        resultTV.setText(result);
+        Log.d(MainActivity.class.getSimpleName(), result);
+        try {
+            JSONObject responseJson = new JSONObject(result);
+            String ip = responseJson.getString("ip");
+            Log.d(MainActivity.class.getSimpleName(), ip);
+            Log.d(MainActivity.class.getSimpleName(),"Vash ip = " + ip);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        super.onPostExecute(result);
+    }
+}
+    private String downloadIpInfo(String address) throws IOException {
+        InputStream inputStream = null;
+        String data = "";
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setReadTimeout(100000);
+            connection.setConnectTimeout(100000);
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(true);
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 200 OK
+                inputStream = connection.getInputStream();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                int read = 0;
+                while ((read = inputStream.read()) != -1) {
+                    bos.write(read);
+                }
+                byte[] result = bos.toByteArray();
+                bos.close();
+                data = new String(result);
+            } else {
+                data = connection.getResponseMessage() + " . Error Code : " + responseCode;
+            }
+            connection.disconnect();
+            //return data;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return data;
+
+
+    }
+}
