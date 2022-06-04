232457a3be8b7ff2d460f006030304be86ff163d
AlexanderKLM <alex57net@mail.ru>
2022-06-04 15:09:51 +0300

    9

---
+++ googlemaps/src/main/java/ru/mirea/khelimskiy/googlemaps/MapsActivity.java
@@ -0,0 +1,116 @@
+package ru.mirea.khelimskiy.googlemaps;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import ru.mirea.khelimskiy.googlemaps.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private Button findmy;
+    private boolean isWork = false;
+    static final int REQUEST_CODE_PERMISSION_FINE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        findmy = findViewById(R.id.buttonWhere);
+
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapClickListener(this);
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+// добавление кнопок изменнеия масштаба
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+// отображение слоя загруженности дорог
+        mMap.setTrafficEnabled(true);
+        setUpMap();
+        // mMap.getUiSettings().setMyLocationButtonEnabled(true);
+    }
+
+    public void Find(View view) {
+        //Prompt the user once explanation has been shown
+        int locPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (locPermissionStatus == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        } else {
+            // Выполняется запрос к пользователь на получение необходимых разрешений
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_PERMISSION_FINE);
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+// производится проверка полученного результата от пользователя на запрос разрешения Camera
+        if (requestCode == REQUEST_CODE_PERMISSION_FINE) {
+            isWork =(grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED);
+        }
+    }
+    private void setUpMap() {
+        // выбираем один вариант
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng mirea = new LatLng(55.670005, 37.479894);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                mirea).zoom(12).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().title("МИРЭА")
+                .snippet("Крупнейший политехнический ВУЗ").position(mirea));
+    }
+    @Override
+    public void onMapClick(LatLng latLng) {
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                latLng).zoom(12).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().title("Где я?")
+                .snippet("Новое место").position(latLng));
+
+    }
+
+}
